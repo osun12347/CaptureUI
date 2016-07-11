909c4122e1ed0512c78a57b693427521e85db998
Neal <Neal@V-DOXIE-W10>
2016-07-11 18:11:03 +0800

    Add project files.

---
+++ CaptureUI/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.Core;
+using Windows.Media.Editing;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CaptureUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaComposition mediaComposition;
+        MediaStreamSource mediaStreamSource;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //pic
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            captureUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+            SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+        BitmapPixelFormat.Bgra8,
+        BitmapAlphaMode.Premultiplied);
+
+            SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+            Img.Source = bitmapSource;
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //video
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.VideoSettings.Format = CameraCaptureUIVideoFormat.Mp4;
+
+            StorageFile videoFile = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Video);
+
+            if (videoFile == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+            mediaComposition = new MediaComposition();
+            MediaClip mediaClip = await MediaClip.CreateFromFileAsync(videoFile);
+
+            mediaComposition.Clips.Add(mediaClip);
+            mediaStreamSource = mediaComposition.GeneratePreviewMediaStreamSource(
+                (int)media.ActualWidth,
+                (int)media.ActualHeight);
+            media.SetMediaStreamSource(mediaStreamSource);
+
+
+        }
+    }
+}
